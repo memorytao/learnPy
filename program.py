6517e2188a799330fd4dd144fd8bd00757d9830e
memorytao <memorytao@gmail.com>
2022-09-26 22:44:53 +0700

    xx

---
+++ program.py
@@ -0,0 +1,14 @@
+from ast import If
+from curses.ascii import NUL
+import random
+import sys
+
+# def predict():
+#     input(" What is your question? : ")
+#     print("Answer is ..... ", random.choice(['Y','N']))
+
+
+# predict()
+x = 0
+x = input()
+print(x)
