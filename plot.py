48b881005e00d1dd11b2a409656dd281ec40ad5a
memorytao <memorytao@gmail.com>
2023-01-24 22:00:48 +0700

    add new filter

--- plot.py
+++ plot.py
@@ -11,30 +11,29 @@ values = df['value']
 
 listOfCDR = cdr.unique().tolist()
 listOfDatas = list(zip(hrs, cdr, values))
-maxOfEachHrs = dict.fromkeys(hrs, 0)
-sumOfEachHrs = {}
 
-for hour in maxOfEachHrs.keys():
-    maxOfEachCDR = dict.fromkeys(listOfCDR, 0)
 
-    for cdr in maxOfEachCDR.keys():
-        findMax = 0
-        for colHrs,colCDR,colVal in listOfDatas:
-            if (findMax == 0 and cdr == colCDR and hour == colHrs):
-                findMax = colVal
-                maxOfEachCDR[cdr] = findMax
-                continue
-            if (cdr == colCDR and hour == colHrs and colVal > findMax ):
-                findMax = colVal
-                maxOfEachCDR[cdr] = findMax
+column1_name = 'CDR'
+column2_name = 'Hour'
+condition = (df[column1_name] == 'MON') & (df[column2_name] == 10)
+filtered_df = df[condition]
+print(filtered_df)
 
-    sumOfEachHrs[hour] = maxOfEachCDR    
+# graph each hours
+
+
+
+
 # print(sumOfEachHrs,end="\n")        
     # Create the line plot
-    plt.subplots(figsize=(12, 6))
-    plt.bar(maxOfEachCDR.keys(), maxOfEachCDR.values())
-    plt.xlabel('CDR')
-    plt.ylabel('value')
-    plt.title('Source By Hour at '+ str(hour))
-    plt.subplots_adjust(left=0.25, right=0.75, bottom=0.25, top=0.75)
-    plt.show()
+    # plt.figure(figsize=(12, 6))
+    
+    # plt.plot(maxOfEachCDR.keys(), maxOfEachCDR.values())
+    # plt.scatter(maxOfEachCDR.keys(), maxOfEachCDR.values())
+    # plt.xlabel('Type of CDR')
+    # plt.ylabel('Number of CDR')
+    # plt.ylim(-10000,999999) 
+    # plt.title('Source By Hour at '+ str(hour))
+    # plt.subplots_adjust(left=0.25, right=0.75, bottom=0.25, top=0.75)
+# plt.show()
+#
