ab0ae160332aee668580c9cb4c42547b9a476d54
memorytao <memorytao@gmail.com>
2023-07-21 11:24:27 +0700

    improve sql

--- pythonSQL.py
+++ pythonSQL.py
@@ -1,17 +1,32 @@
 import sqlalchemy as db
 import pandas as pd
 
+tables = {
+    "Album": 1,
+    "Artist": 1,
+    "Customer": 1,
+    "Employee": 1,
+    "Genre": 1,
+    "Invoice": 1,
+    "InvoiceLine": 1,
+    "MediaType": 1,
+    "Playlist": 1,
+    "PlaylistTrack": 1,
+    "Track": 1,
+}
+
 # Create a connection to the database
 driver_url = "sqlite:///C:/Users/Tao/AppData/Roaming/DBeaverData/workspace6/.metadata/sample-database-sqlite-1/Chinook.db"
 engine = db.create_engine(driver_url)
-
+sql = 'SELECT * FROM {}'
 # Query the data
 with engine.connect() as conn:
-    query = db.text('SELECT * FROM Customer')
-    result = conn.execute(query)
+    for table in tables:
+        query = db.text(sql.format(table))
+        result = conn.execute(query)
 
-    # Create a Pandas DataFrame from the results of the SQL query
-    df = pd.DataFrame(result)
+        # Create a Pandas DataFrame from the results of the SQL query
+        df = pd.DataFrame(result)
 
-    # Print the first names of all the customers
-    print(df['FirstName'].to_string(index=False))
+        # Print the first names of all the customers
+        print(df.to_string(index=False))
