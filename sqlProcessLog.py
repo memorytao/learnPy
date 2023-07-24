a82b52de44d0964bf537d96d353cabe9bd0f1285
memorytao <memorytao@gmail.com>
2023-07-24 13:36:52 +0700

    update 24 Jul 2023

--- sqlProcessLog.py
+++ sqlProcessLog.py
@@ -2,55 +2,66 @@ import sqlalchemy as db
 import pandas as pd
 from datetime import datetime
 from datetime import timedelta
+import sys
 
-TABLES = {
-    "CVM_HOUSE_HOLD_IDCARD_ADDR": -1,
-    "DIM_ACCT": -1,
-    "DIM_CELLSITE_LOCATION": -1,
-    "DIM_CUST": -1,
-    "DIM_HANDSETS": -1,
-    "DIM_ORDR_ACTVTN": -1,
-    "DIM_PRICE_PLAN_MV": -1,
-    "DIM_PROD": -1,
-    "DIM_PROD_5G": -1,
-    "DIM_PROD_EXT": -1,
-    "DIM_RC_RATE": -1,
-    "DIM_TOL_PROMOTION": -1,
-    "DIM_TRUEID": -2,
-    "Employee": -1,
-    "FCT_CHRG": -1,
-    "FCT_INVC": -1,
-    "FCT_INVC_PROD": -1,
-    "FCT_POST_CHARGE": -1,
-    "FCT_PREP_SUBS_ACTIVATE": -1,
-    "FCT_PYMT": -1,
-    "FCT_SUBS_SERVICE_NMODEL": -1,
-    "FCT_TOPUP": -2,
-    "FCT_TRUEID_UNLCK": -2,
-    "FCT_VAS_CONTENT_NMODEL": -2
-}
-
-CMPGN_MASTER_PROCESS_LOG_COLUMNS = ['Day_ID', 'Round', 'Schema_Name',
-                                    'Table_Name', 'Latest_Date', 'Data_Amount', 'Status', 'Create_DTTM']
 
 # Create a connection to the database
-# DRIVER_URL = "sqlite:///C:/Users/Tao/AppData/Roaming/DBeaverData/workspace6/.metadata/sample-database-sqlite-1/Chinook.db"
-DRIVER_URL = "sqlite:////Users/memorytao/Library/DBeaverData/workspace6/.metadata/sample-database-sqlite-1/Chinook.db"
+DRIVER_URL = "sqlite:///C:/Users/Tao/AppData/Roaming/DBeaverData/workspace6/.metadata/sample-database-sqlite-1/Chinook.db"
+# DRIVER_URL = "sqlite:////Users/memorytao/Library/DBeaverData/workspace6/.metadata/sample-database-sqlite-1/Chinook.db"
 engine = db.create_engine(DRIVER_URL)
 currentTime = datetime.now()
+currTimeStr = currentTime.strftime("%Y-%m-%d %H:%M:%S")
 
-SQL = 'SELECT "NZDATAMART" as SCHEMA ,* FROM {} LIMIT 10'
+
+# ROUND_AT_DAY = sys.argv[1] # round of the day (eg. 1 = 9:00 , 2 = 12:00, 3 = 15:00)
+ROUND_AT_DAY = 1
+
+INSERT_TABLE = "CVM_CMPGN_MASTER_PROCESS_LOG"
+SQL = 'SELECT * FROM CVM_CMPGN_MASTER_TABLE ccmt'
+
+DAY_ID = []
+ROUND = []
+SCHEMA_NAME = []
+TABLE_NAME = []
+LATEST_DATE = []
+DATA_AMOUNT = []
+STATUS = []
+CREATE_DTTM = []
+
 
 # Query the data
+i = 0;
 with engine.connect() as conn:
-    for table in TABLES:
-        timeBefore = currentTime + timedelta(days=TABLES[table])
 
-        query = db.text(SQL.format(table))
-        result = conn.execute(query)
-        # Create a Pandas DataFrame from the results of the SQL query
-        df = pd.DataFrame(result)
-        # Replace None with "Unknown" in all columns
-        df.replace(to_replace='None', value='Unknown', inplace=True)
-        # Print the first names of all the customers
-        df.to_excel('./{}.xlsx'.format(table), index=False)
+    query = db.text(SQL)
+    result = conn.execute(query)
+    # Create a Pandas DataFrame from the results of the SQL query
+    df = pd.DataFrame(result)
+    
+    # Replace None with "Unknown" in all columns
+    df.replace(to_replace='None', value='', inplace=True)
+
+    # DAY_ID.append(df['CHECK_FIELD_NAME_1'])
+    # ROUND.append(ROUND_AT_DAY)
+    SCHEMA_NAME.append(df['SCHEMA_NAME'])
+    TABLE_NAME.append(df['TABLE_NAME'])
+    # LATEST_DATE.append(ROUND_AT_DAY)
+    # DATA_AMOUNT.append('')
+    # STATUS.append('')
+    # CREATE_DTTM.append(currTimeStr)
+
+print(i)
+data_to_process_log_table = pd.DataFrame(
+    {
+        'DAY_ID': DAY_ID,
+        'ROUND': ROUND,
+        'SCHEMA_NAME': SCHEMA_NAME,
+        'TABLE_NAME': TABLE_NAME,
+        'LATEST_DATE': LATEST_DATE,
+        'DATA_AMOUNT': DATA_AMOUNT,
+        'STATUS': STATUS,
+        'CREATE_DTTM': CREATE_DTTM,
+    }
+)
+data_to_process_log_table.to_sql(
+    name=INSERT_TABLE, con=engine, index=False, if_exists='append')
