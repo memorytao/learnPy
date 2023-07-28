0673b1435c1389972fb343efdc7b3977a5ea9b2d
memorytao <memorytao@gmail.com>
2023-07-28 16:58:56 +0700

    update for process log

--- sqlMasterTable.py
+++ sqlMasterTable.py
@@ -1,56 +1,261 @@
-import sqlalchemy as db
+#!/usr/bin/python3
+
+
+import os
+import sys
+import re
+import logging
+import subprocess
 import pandas as pd
+import nzpy as nz
 from datetime import datetime
-from datetime import timedelta
 
-TABLES = {
-    "CVM_HOUSE_HOLD_IDCARD_ADDR": -1,
-    "DIM_ACCT": -1,
-    "DIM_CELLSITE_LOCATION": -1,
-    "DIM_CUST": -1,
-    "DIM_HANDSETS": -1,
-    "DIM_ORDR_ACTVTN": -1,
-    "DIM_PRICE_PLAN_MV": -1,
-    "DIM_PROD": -1,
-    "DIM_PROD_5G": -1,
-    "DIM_PROD_EXT": -1,
-    # "DIM_RC_RATE": -1,
-    "DIM_TOL_PROMOTION": -1,
-    "DIM_TRUEID": -2,
-    "Employee": -1,
-    "FCT_CHRG": -1,
-    "FCT_INVC": -1,
-    "FCT_INVC_PROD": -1,
-    "FCT_POST_CHARGE": -1,
-    "FCT_PREP_SUBS_ACTIVATE": -1,
-    "FCT_PYMT": -1,
-    "FCT_SUBS_SERVICE_NMODEL": -1,
-    "FCT_TOPUP": -2,
-    "FCT_TRUEID_UNLCK": -2,
-    "FCT_VAS_CONTENT_NMODEL": -2
-}
+subprocess.check_call([sys.executable, "-m", "pip",
+                      "install", "nzpy", "--user"])
 
-CMPGN_MASTER_COLUMNS = ['Schema_Name', 'Table_Name', 'Table_Description', 'Table_Short_Description ', 'SLA_Data', 'SLA_Time',
-                        'Min_Data_Threshold', 'Max_Data_Threshold', 'Check_Field_Name_1', 'Check_Field_Name_2', 'Update_DTTM', 'Update_By']
+CSV_PATH = ""
+TMP_PATH = ""
+FORMAT_YYYYMMDD = "%Y%m%d"
+FORMAT_YYYYMMDD_HHMMSS = "%Y/%m/%d %H:%M:%S"
+FORMAT_YYMMDD = "%y%m%d"
+CURRENT_DATE = ""
+ROUND_JOB = sys.argv[1]
+TARGET_DIRECTORY = "/path/to/your/csv/files/directory"
 
-# Create a connection to the database
-# DRIVER_URL = "sqlite:///C:/Users/Tao/AppData/Roaming/DBeaverData/workspace6/.metadata/sample-database-sqlite-1/Chinook.db"
-DRIVER_URL = "sqlite:////Users/memorytao/Library/DBeaverData/workspace6/.metadata/sample-database-sqlite-1/Chinook.db"
-engine = db.create_engine(DRIVER_URL)
-currentTime = datetime.now()
+# Data containers
+DAY_ID_DATA = []
+ROUND_DATA = []
+SCHEMA_NAME_DATA = []
+TABLE_NAME_DATA = []
+TABLE_DESC_DATA = []
+SLA_DATA_T = []
+LATEST_DATE_DATA = []
+DATA_AMOUNT_DATA = []
+STATUS_DATA = []
+CREATE_DTTM_DATA = []
 
-SQL = 'SELECT "NZDATAMART" as SCHEMA ,* FROM {} LIMIT 10'
+LIST_SLECTED_COLUMN = ["SCHEMA_NAME", "TABLE_NAME", "TABLE_DESCRIPTION", "TABLE_SHORT_DESCRIPTION",
+                       "TABLE_CATEGORY", "SLA_DATA", "SLA_TIME", "MIN_DATA_THRESHOLD",
+                       "MAX_DATA_THRESHOLD", "CHECK_FIELD_NAME_1", "DATA_TYPE", "CHECK_FIELD_NAME_2",
+                       "CHECK_FIELD_NAME_3", "CHECK_FIELD_NAME_4", "UPDATE_DTTM", "UPDATE_BY"]
 
-# Query the data
-with engine.connect() as conn:
-    for table in TABLES:
-        timeBefore = currentTime + timedelta(days=TABLES[table])
+INSERT_TABLE = "CVM_CMPGN_MASTER_PROCESS_LOG"
 
-        query = db.text(SQL.format(table))
-        result = conn.execute(query)
-        # Create a Pandas DataFrame from the results of the SQL query
-        df = pd.DataFrame(result)
-        # Replace None with "Unknown" in all columns
-        df.replace(to_replace='None', value='Unknown', inplace=True)
-        # Print the first names of all the customers
-        df.to_excel('./{}.xlsx'.format(table), index=False)
+
+def get_connection():
+    try:
+        USER_NAME = "T968672"
+        PASSWORD = "sa4+VdllVLi$UkV"
+        HOST = "10.50.78.21"
+        POST = 5480
+        DATABASE = "NZDATAMART"
+        return nz.connect(user=USER_NAME, password=PASSWORD, host=HOST, port=POST, database=DATABASE).cursor()
+    except Exception as err:
+        print('error to connect the database ', type(err), err)
+        logging.error('error to connect the database')
+        logging.error('please check your connection')
+
+
+def sql_cvm_cmpgn_master():
+    sql = """SELECT SCHEMA_NAME,TABLE_NAME,TABLE_DESCRIPTION,TABLE_SHORT_DESCRIPTION, 
+            TABLE_CATEGORY,SLA_DATA, SLA_TIME,MIN_DATA_THRESHOLD,
+            MAX_DATA_THRESHOLD,CHECK_FIELD_NAME_1,DATA_TYPE,CHECK_FIELD_NAME_2, 
+            CHECK_FIELD_NAME_3,CHECK_FIELD_NAME_4,UPDATE_DTTM,UPDATE_BY
+            FROM CVM_CMPGN_MASTER_TABLE 
+        """
+    return sql
+
+
+def insert_to_process_log(current_time_yyyymmdd, schema_name, table_name, latest_date, amount_sub, status):
+    logging.info('inserting data to database....')
+    try:
+        cursor.execute("insert into {} values (?,?,?,?,?,?,?,?)".format(INSERT_TABLE),
+                       (current_time_yyyymmdd, ROUND_JOB, schema_name, table_name, latest_date, amount_sub, status, datetime.now().strftime(FORMAT_YYYYMMDD_HHMMSS)))
+
+    except Exception as e:
+        print("error occurred during insertion: ", e)
+
+
+def create_csv():
+    logging.info('creating csv file....')
+    try:
+        # Create a DataFrame with the collected data
+        data_to_process_log_table = pd.DataFrame({
+            'DAY_ID': DAY_ID_DATA,
+            'ROUND': ROUND_DATA,
+            'SCHEMA_NAME': SCHEMA_NAME_DATA,
+            'TABLE_NAME': TABLE_NAME_DATA,
+            'TABLE_DESCRIPTION': TABLE_DESC_DATA,
+            'SLA_DATA': SLA_DATA_T,
+            'LATEST_DATE': LATEST_DATE_DATA,
+            'DATA_AMOUNT': DATA_AMOUNT_DATA,
+            'STATUS': STATUS_DATA,
+            'CREATE_DTTM': CREATE_DTTM_DATA,
+        })
+        # Save data to a CSV file
+        create_date = datetime.now().strftime(FORMAT_YYYYMMDD)
+        data_to_process_log_table.to_csv(
+            './{}_{}_{}.csv'.format(INSERT_TABLE, create_date, ROUND_JOB), index=False, sep="|", header=None)
+
+    except Exception as err:
+        print('create csv error', type(err), err)
+        logging.error('create csv error')
+    logging.info('created csv file!')
+
+
+def send_mail():
+    # Commented out section. Check if it's required, otherwise it can be removed.
+    logging.info('sending email.... ')
+    try:
+        is_dir = os.path.isdir(CSV_PATH)
+        if (not is_dir):
+            os.mkdir(CSV_PATH)
+            cmd = "(sh /data/CVM/monitor/script/file_name.sh) & "
+            os.system(cmd)
+        logging.info('email has sent! ')
+
+    except Exception as err:
+        print("error sending email ", type, err)
+        logging.error('send email error! ')
+    sys.exit(0)
+
+
+def get_file_creation_date(file_name):
+    # Extract the date from the file name using the YYYYMMDD pattern
+    match = re.search(r"\d{8}", file_name)
+    if match:
+        date_str = match.group()
+        return datetime.datetime.strptime(date_str, "%Y%m%d").date()
+    return None
+
+def remove_csv_files(directory):
+    # Get the current date
+    current_date = datetime.date.today()
+
+    # List all files in the directory
+    files = os.listdir(directory)
+
+    # Filter and remove CSV files older than 7 days
+    for file_name in files:
+        if file_name.lower().endswith(".csv"):
+            creation_date = get_file_creation_date(file_name)
+            if creation_date:
+                file_age = (current_date - creation_date).days
+                if file_age > 7:
+                    file_path = os.path.join(directory, file_name)
+                    os.remove(file_path)
+                    print("Removed ",file_name)
+
+# create form daily insert to CVM_CMPGN_MASTER_PROCESS_LOG
+
+
+def create_report_form(schema_name, table_name, table_description, sla_data, latest_date, amount_sub, status):
+    try:
+        current_time_yyyymmdd = datetime.now().strftime(FORMAT_YYYYMMDD)
+        create_datetime = datetime.now().strftime(FORMAT_YYYYMMDD_HHMMSS)
+        DAY_ID_DATA.append(current_time_yyyymmdd)
+        ROUND_DATA.append(ROUND_JOB)
+        SCHEMA_NAME_DATA.append(schema_name)
+        TABLE_NAME_DATA.append(table_name)
+        TABLE_DESC_DATA.append(table_description)
+        SLA_DATA_T.append(sla_data)
+        LATEST_DATE_DATA.append(latest_date)
+        DATA_AMOUNT_DATA.append(amount_sub)
+        STATUS_DATA.append(status)
+        CREATE_DTTM_DATA.append(create_datetime)
+
+        # insert to CVM_CMPGN_MASTER_PROCESS_LOG
+        insert_to_process_log(current_time_yyyymmdd=current_time_yyyymmdd, schema_name=schema_name,
+                              table_name=table_name, latest_date=latest_date, amount_sub=amount_sub, status=status)
+    except Exception as err:
+        print('create report error! ', type(err), err)
+        logging.error('')
+
+
+def check_latest_updated(check_field_name_1, table_name):
+
+    logging.info('status is checking....')
+    try:
+        # SQL to get the latest date from the table
+        res = {
+            'status': '',
+            'amount': 0,
+            'latest_date':''
+        }
+        SQL_CHECK_LATEST = """SELECT to_char(MAX(DATE({})),'YYYYMMDD') AS LOADDATE FROM {} ;"""
+
+        db_latest_time = cursor.execute(
+            SQL_CHECK_LATEST.format(check_field_name_1, table_name))
+        latest_date = db_latest_time.fetchall()[0][0]
+        current_time = datetime.strptime(
+            datetime.now().strftime(FORMAT_YYYYMMDD), FORMAT_YYYYMMDD)
+        latest_update_date = datetime.strptime(latest_date, FORMAT_YYYYMMDD)
+
+        SQL_COUNT_AMOUNT = """SELECT COUNT(1) FROM {} """.format(table_name)
+
+        if TABLE_CATEGORY == 'TRANSACTION':
+            SQL_COUNT_AMOUNT += "WHERE to_char(DATE({}),'YYYYMMDD') = '{}' ".format(
+                check_field_name_1, latest_update_date.strftime(FORMAT_YYYYMMDD))
+
+        max_sub = int(MAX_DATA_THRESHOLD)
+        min_sub = int(MIN_DATA_THRESHOLD)
+
+        db_amount = cursor.execute(SQL_COUNT_AMOUNT)
+        amount_sub = int(db_amount.fetchall()[0][0])
+        res['amount'] = amount_sub
+        res['latest_date'] = latest_date
+
+        # Determine the status based on the current date and the latest update date
+        if (current_time == latest_update_date and (amount_sub >= min_sub and amount_sub <= max_sub)):
+            res['status'] = "Normal"
+        elif (current_time > latest_update_date):
+            res['status'] = "Delay"
+        else:
+            res['status'] = "AbNormal"
+
+        return res
+    except Exception as err:
+        print('check status error!', type(err), err)
+        logging.error('check status error!')
+
+
+with get_connection() as cursor:
+    logging.info('connected to database')
+
+    cursor.execute(sql_cvm_cmpgn_master())
+    cvm_campaign_from_sql = cursor.fetchall()
+    cvm_master_data = pd.DataFrame(
+        cvm_campaign_from_sql, columns=LIST_SLECTED_COLUMN)
+
+    try:
+        for idx, master in cvm_master_data.iterrows():
+            SCHEMA_NAME = master['SCHEMA_NAME']
+            TABLE_NAME = master['TABLE_NAME']
+            TABLE_DESCRIPTION = master['TABLE_DESCRIPTION']
+            TABLE_SHORT_DESCRIPTION = master['TABLE_SHORT_DESCRIPTION']
+            TABLE_CATEGORY = master['TABLE_CATEGORY']
+            SLA_DATA = master['SLA_DATA']
+            SLA_TIME = master['SLA_TIME']
+            MAX_DATA_THRESHOLD = master['MAX_DATA_THRESHOLD']
+            MIN_DATA_THRESHOLD = master['MIN_DATA_THRESHOLD']
+            CHECK_FIELD_NAME_1 = master['CHECK_FIELD_NAME_1']
+            DATA_TYPE = master['DATA_TYPE']
+            CHECK_FIELD_NAME_2 = master['CHECK_FIELD_NAME_2']
+            CHECK_FIELD_NAME_3 = master['CHECK_FIELD_NAME_3']
+            CHECK_FIELD_NAME_4 = master['CHECK_FIELD_NAME_4']
+            UPDATE_DTTM = master['UPDATE_DTTM']
+            UPDATE_BY = master['UPDATE_BY']
+
+            if TABLE_NAME == 'FCT_INVC_PROD':
+                # Skip the FCT_INVC_PROD table
+                continue
+
+            res = check_latest_updated(check_field_name_1=CHECK_FIELD_NAME_1, table_name=TABLE_NAME)
+
+            create_report_form(schema_name=SCHEMA_NAME, table_name=TABLE_NAME, table_description=TABLE_DESC_DATA,
+                               sla_data=SLA_DATA, latest_date=res['latest_date'], 
+                               amount_sub=res['amount'], status=res['status'])
+
+            # remove_csv_files(TARGET_DIRECTORY)
+    except Exception as err:
+        print("Error occurs ", err, "error type", type(err))
--- sqlProcessLog.py
+++ sqlProcessLog.py
@@ -1,13 +1,21 @@
-import sqlalchemy as db
-import pandas as pd
-from datetime import datetime
-import sys
+#!/usr/bin/python3
 import os
+import sys
+from datetime import datetime
+import pandas as pd
+import subprocess
+import logging
+
+subprocess.check_call([sys.executable, "-m", "pip",
+                      "install", "nzpy", "--user"])
+import nzpy as nz
 
 
 CSV_PATH = ""
 TMP_PATH = ""
-DATE_FORMAT = "%Y%m%d"
+FORMAT_YYYYMMDD = "%Y%m%d"
+FORMAT_YYYYMMDD_HHMMSS = "%Y/%m/%d %H:%M:%S"
+FORMAT_YYMMDD = "%y%m%d"
 round_job = sys.argv[1]
 
 # Data containers
@@ -15,6 +23,8 @@ DAY_ID_DATA = []
 ROUND_DATA = []
 SCHEMA_NAME_DATA = []
 TABLE_NAME_DATA = []
+TABLE_DESC_DATA = []
+SLA_DATA_T = []
 LATEST_DATE_DATA = []
 DATA_AMOUNT_DATA = []
 STATUS_DATA = []
@@ -22,86 +32,143 @@ CREATE_DTTM_DATA = []
 
 INSERT_TABLE = "CVM_CMPGN_MASTER_PROCESS_LOG"
 
-# Create a connection to the database
-DRIVER_URL = "netezza+nzpy://T968672:sa4+VdllVLi$UkV@10.50.78.21:5480/NZDATAMART"
-engine = db.create_engine(DRIVER_URL)
+USER_NAME = "T968672"
+PASSWORD = "sa4+VdllVLi$UkV"
+HOST = "10.50.78.21"
+POST = 5480
+DATABASE = "NZDATAMART"
 
-SQL_CVM_CMPGN_MASTER_TABLE = """
+
+def send_mail():
+    # Commented out section. Check if it's required, otherwise it can be removed.
+    try:
+        is_dir = os.path.isdir(CSV_PATH)
+        if (not is_dir):
+            os.mkdir(CSV_PATH)
+            cmd = "(sh /data/CVM/monitor/script/file_name.sh) & "
+            os.system(cmd)
+    except Exception as err:
+        print(f"Error occurs :::  {err=}, {type(err)=}")
+    sys.exit(0)
+
+
+conn = nz.connect(
+    user=USER_NAME,
+    password=PASSWORD,
+    host=HOST,
+    port=POST,
+    database=DATABASE
+)
+
+
+def sql_cvm_cmpgn_master():
+    sql = """SELECT SCHEMA_NAME,TABLE_NAME,TABLE_DESCRIPTION,TABLE_SHORT_DESCRIPTION, 
+            TABLE_CATEGORY,SLA_DATA, SLA_TIME,MIN_DATA_THRESHOLD,
+            MAX_DATA_THRESHOLD,CHECK_FIELD_NAME_1,DATA_TYPE,CHECK_FIELD_NAME_2, 
+            CHECK_FIELD_NAME_3,CHECK_FIELD_NAME_4,UPDATE_DTTM,UPDATE_BY
+            FROM CVM_CMPGN_MASTER_TABLE 
+        """
+    return sql
+
+with conn.cursor() as cursor:
+    SQL_CVM_CMPGN_MASTER_TABLE = """
     SELECT SCHEMA_NAME, TABLE_NAME, TABLE_DESCRIPTION, TABLE_SHORT_DESCRIPTION, TABLE_CATEGORY, SLA_DATA, SLA_TIME,
-           MIN_DATA_THRESHOLD, MAX_DATA_THRESHOLD, CHECK_FIELD_NAME_1, DATA_TYPE, CHECK_FIELD_NAME_2, 
-           CHECK_FIELD_NAME_3, CHECK_FIELD_NAME_4, UPDATE_DTTM, UPDATE_BY
+        MIN_DATA_THRESHOLD, MAX_DATA_THRESHOLD, CHECK_FIELD_NAME_1, DATA_TYPE, CHECK_FIELD_NAME_2, 
+        CHECK_FIELD_NAME_3, CHECK_FIELD_NAME_4, UPDATE_DTTM, UPDATE_BY
     FROM CVM_CMPGN_MASTER_TABLE 
 """
-
-with engine.connect() as conn:
-    cvm_campaign_from_sql = conn.execute(db.text(SQL_CVM_CMPGN_MASTER_TABLE))
+    cursor.execute(SQL_CVM_CMPGN_MASTER_TABLE)
+    cvm_campaign_from_sql = cursor.fetchall()
     cvm_master_data = pd.DataFrame(cvm_campaign_from_sql,
-                                   columns=[
-                                       "SCHEMA_NAME", "TABLE_NAME", "TABLE_DESCRIPTION", "TABLE_SHORT_DESCRIPTION",
-                                       "TABLE_CATEGORY", "SLA_DATA", "SLA_TIME", "MIN_DATA_THRESHOLD",
-                                       "MAX_DATA_THRESHOLD", "CHECK_FIELD_NAME_1", "DATA_TYPE", "CHECK_FIELD_NAME_2",
-                                       "CHECK_FIELD_NAME_3", "CHECK_FIELD_NAME_4", "UPDATE_DTTM", "UPDATE_BY"
-                                   ]
-                                   )
+                                    columns=[
+                                        "SCHEMA_NAME", "TABLE_NAME", "TABLE_DESCRIPTION", "TABLE_SHORT_DESCRIPTION",
+                                        "TABLE_CATEGORY", "SLA_DATA", "SLA_TIME", "MIN_DATA_THRESHOLD",
+                                        "MAX_DATA_THRESHOLD", "CHECK_FIELD_NAME_1", "DATA_TYPE", "CHECK_FIELD_NAME_2",
+                                        "CHECK_FIELD_NAME_3", "CHECK_FIELD_NAME_4", "UPDATE_DTTM", "UPDATE_BY"
+                                    ]
+                                    )
 
-    for idx, master in cvm_master_data.iterrows():
-        TABLE_CATEGORY = master['TABLE_CATEGORY']
-        DATA_TYPE = master['DATA_TYPE']
-        CHECK_FIELD_NAME_1 = master['CHECK_FIELD_NAME_1']
-        TABLE_NAME = master['TABLE_NAME']
-        MAX_DATA_THRESHOLD = master['MAX_DATA_THRESHOLD']
-        MIN_DATA_THRESHOLD = master['MIN_DATA_THRESHOLD']
-        SCHEMA_NAME = master['SCHEMA_NAME']
+    # print(cvm_master_data.to_string(index=False))
+    try:
+        for idx, master in cvm_master_data.iterrows():
+            SCHEMA_NAME = master['SCHEMA_NAME']
+            TABLE_NAME = master['TABLE_NAME']
+            TABLE_DESCRIPTION = master['TABLE_DESCRIPTION']
+            TABLE_SHORT_DESCRIPTION = master['TABLE_SHORT_DESCRIPTION']
+            TABLE_CATEGORY = master['TABLE_CATEGORY']
+            SLA_DATA = master['SLA_DATA']
+            SLA_TIME = master['SLA_TIME']
+            MAX_DATA_THRESHOLD = master['MAX_DATA_THRESHOLD']
+            MIN_DATA_THRESHOLD = master['MIN_DATA_THRESHOLD']
+            CHECK_FIELD_NAME_1 = master['CHECK_FIELD_NAME_1']
+            DATA_TYPE = master['DATA_TYPE']
+            CHECK_FIELD_NAME_2 = master['CHECK_FIELD_NAME_2']
+            CHECK_FIELD_NAME_3 = master['CHECK_FIELD_NAME_3']
+            CHECK_FIELD_NAME_4 = master['CHECK_FIELD_NAME_4']
+            UPDATE_DTTM = master['UPDATE_DTTM']
+            UPDATE_BY = master['UPDATE_BY']
 
-        # if TABLE_NAME == 'FCT_INVC_PROD':
-        #     # Skip the FCT_INVC_PROD table
-        #     continue
+            if TABLE_NAME == 'FCT_INVC_PROD':
+                # Skip the FCT_INVC_PROD table
+                continue
 
-        # SQL to get the latest date from the table
-        SQL_CHECK_LATEST = """SELECT to_char(MAX(DATE({})),'YYYYMMDD') AS LOADDATE FROM {} ;"""
-        db_latest_time = conn.execute(
-            db.text(SQL_CHECK_LATEST.format(CHECK_FIELD_NAME_1, TABLE_NAME)))
-        latest_date = db_latest_time.fetchall()[0][0]
-        currentTime = datetime.strptime(
-            datetime.now().strftime(DATE_FORMAT), DATE_FORMAT)
-        latest_update_date = datetime.strptime(latest_date, DATE_FORMAT)
+            # SQL to get the latest date from the table
+            SQL_CHECK_LATEST = """SELECT to_char(MAX(DATE({})),'YYYYMMDD') AS LOADDATE FROM {} ;"""
 
-        SQL_COUNT_AMOUNT = """SELECT COUNT(1) FROM {} """.format(TABLE_NAME)
+            db_latest_time = cursor.execute(
+                SQL_CHECK_LATEST.format(CHECK_FIELD_NAME_1, TABLE_NAME))
+            latest_date = db_latest_time.fetchall()[0][0]
+            current_time = datetime.strptime(
+                datetime.now().strftime(FORMAT_YYYYMMDD), FORMAT_YYYYMMDD)
+            latest_update_date = datetime.strptime(latest_date, FORMAT_YYYYMMDD)
 
-        if TABLE_CATEGORY == 'TRANSACTION':
-            SQL_COUNT_AMOUNT += "WHERE to_char(DATE({}),'YYYYMMDD') = '{}' ".format(
-                CHECK_FIELD_NAME_1, latest_update_date.strftime(DATE_FORMAT))
+            SQL_COUNT_AMOUNT = """SELECT COUNT(1) FROM {} """.format(
+                TABLE_NAME)
 
-        max_sub = int(MAX_DATA_THRESHOLD)
-        min_sub = int(MIN_DATA_THRESHOLD)
+            if TABLE_CATEGORY == 'TRANSACTION':
+                SQL_COUNT_AMOUNT += "WHERE to_char(DATE({}),'YYYYMMDD') = '{}' ".format(
+                    CHECK_FIELD_NAME_1, latest_update_date.strftime(FORMAT_YYYYMMDD))
 
-        db_amount = conn.execute(db.text(SQL_COUNT_AMOUNT))
-        amount_sub = int(db_amount.fetchall()[0][0])
+            max_sub = int(MAX_DATA_THRESHOLD)
+            min_sub = int(MIN_DATA_THRESHOLD)
 
-        # Determine the status based on the current date and the latest update date
-        status = ""
-        if (currentTime == latest_update_date and (amount_sub >= min_sub and amount_sub <= max_sub)):
-            status = "Normal"
-        elif (currentTime > latest_update_date):
-            status = "Delay"
-        else:
-            status = "AbNormal"
+            db_amount = cursor.execute(SQL_COUNT_AMOUNT)
+            amount_sub = int(db_amount.fetchall()[0][0])
 
-        print('latest:', latest_date, 'check latest:',
-              SQL_CHECK_LATEST.format(CHECK_FIELD_NAME_1, TABLE_NAME))
-        print('amt:', SQL_COUNT_AMOUNT, 'amount:', amount_sub)
-        print('table cate:', TABLE_CATEGORY, 'table:', TABLE_NAME,
-              'max:', max_sub, 'min:', min_sub, 'status:', status)
+            # Determine the status based on the current date and the latest update date
+            status = ""
+            if (current_time == latest_update_date and (amount_sub >= min_sub and amount_sub <= max_sub)):
+                status = "Normal"
+            elif (current_time > latest_update_date):
+                status = "Delay"
+            else:
+                status = "AbNormal"
 
-        # Append data to the containers
-        DAY_ID_DATA.append(currentTime.strftime(DATE_FORMAT))
-        ROUND_DATA.append(round_job)
-        SCHEMA_NAME_DATA.append(SCHEMA_NAME)
-        TABLE_NAME_DATA.append(TABLE_NAME)
-        LATEST_DATE_DATA.append(latest_date)
-        DATA_AMOUNT_DATA.append(amount_sub)
-        STATUS_DATA.append(status)
-        CREATE_DTTM_DATA.append(datetime.now())
+
+            # Append data to the containers
+            current_time_yyyymmdd = current_time.strftime(FORMAT_YYYYMMDD)
+            create_datetime = datetime.now().strftime(FORMAT_YYYYMMDD_HHMMSS)
+            DAY_ID_DATA.append(current_time_yyyymmdd)
+            ROUND_DATA.append(round_job)
+            SCHEMA_NAME_DATA.append(SCHEMA_NAME)
+            TABLE_NAME_DATA.append(TABLE_NAME)
+            TABLE_DESC_DATA.append(TABLE_DESCRIPTION)
+            SLA_DATA_T.append(SLA_DATA)
+            LATEST_DATE_DATA.append(latest_date)
+            DATA_AMOUNT_DATA.append(amount_sub)
+            STATUS_DATA.append(status)
+            CREATE_DTTM_DATA.append(create_datetime)
+
+            # Insert data to the database table
+            try:
+                cursor.execute("insert into {} values (?,?,?,?,?,?,?,?)".format(INSERT_TABLE),
+                            (current_time_yyyymmdd, round_job, SCHEMA_NAME, TABLE_NAME, latest_date, amount_sub, status, datetime.now().strftime(FORMAT_YYYYMMDD_HHMMSS)))
+            except Exception as e:
+                print("Error occurred during insertion: ", e)
+
+    except Exception as err:
+        print("Error occurs ::: ", err, "error type", type(err))
+
 
 # Create a DataFrame with the collected data
 data_to_process_log_table = pd.DataFrame({
@@ -109,27 +176,18 @@ data_to_process_log_table = pd.DataFrame({
     'ROUND': ROUND_DATA,
     'SCHEMA_NAME': SCHEMA_NAME_DATA,
     'TABLE_NAME': TABLE_NAME_DATA,
+    'TABLE_DESCRIPTION': TABLE_DESC_DATA,
+    'SLA_DATA': SLA_DATA_T,
     'LATEST_DATE': LATEST_DATE_DATA,
     'DATA_AMOUNT': DATA_AMOUNT_DATA,
     'STATUS': STATUS_DATA,
     'CREATE_DTTM': CREATE_DTTM_DATA,
 })
 
-# Insert data to the database table
-data_to_process_log_table.to_sql(
-    name=INSERT_TABLE, con=engine, index=False, if_exists='append')
-
 # Save data to a CSV file
-data_to_process_log_table.to_csv('./{}_{}_{}.csv'.format(INSERT_TABLE, datetime.now().strftime(DATE_FORMAT), round_job),
+data_to_process_log_table.to_csv('./{}_{}_{}.csv'.format(INSERT_TABLE, datetime.now().strftime(FORMAT_YYYYMMDD), round_job),
                                  index=False, sep="|", header=None)
 
-# Commented out section. Check if it's required, otherwise it can be removed.
-try:
-    isDir = os.path.isdir(CSV_PATH)
-    if (not isDir):
-        os.mkdir(CSV_PATH)
-    
-except FileExistsError:
-    print(" File or Dir error")
 
-sys.exit(0)
+
+
