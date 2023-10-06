a5391733576ce837a4913070b7b49b14face22df
memorytao <memorytao@gmail.com>
2023-10-06 19:00:38 +0700

    upload file to google drive

---
+++ upalod_to_drive.py
@@ -0,0 +1,27 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+
+# Load credentials from the JSON file you downloaded
+credentials = service_account.Credentials.from_service_account_file(
+    './credentials.json', scopes=['https://www.googleapis.com/auth/drive']
+)
+
+# Create a Google Drive API client
+drive_service = build('drive', 'v3', credentials=credentials)
+
+def upload_file(file_path, folder_id=None):
+    file_metadata = {
+        'name': 'READMEs.md',  # Replace with the desired file name
+        'parents': [folder_id] if folder_id else []
+    }
+
+    media = MediaFileUpload(file_path, resumable=True)
+    file = drive_service.files().create(
+        body=file_metadata, media_body=media, fields='id',
+    ).execute()
+
+    print(f'File ID: {file.get("id")}')
+
+# Example usage:
+upload_file('./README.md', folder_id='19MTjDhfJigonI-e9tBhuGDcODiJXTWht')
