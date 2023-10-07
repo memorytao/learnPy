5c1e093f8d98690463814499c73502d6c1a9a6f2
memorytao <memorytao@gmail.com>
2023-10-07 18:38:04 +0700

    fix upload

--- upalod_to_drive.py
+++ upalod_to_drive.py
@@ -1,18 +1,20 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+from datetime import datetime
 
 # Load credentials from the JSON file you downloaded
 credentials = service_account.Credentials.from_service_account_file(
     './credentials.json', scopes=['https://www.googleapis.com/auth/drive']
 )
 
+name = datetime.today().strftime('%c')
 # Create a Google Drive API client
 drive_service = build('drive', 'v3', credentials=credentials)
 
 def upload_file(file_path, folder_id=None):
     file_metadata = {
-        'name': 'READMEs.md',  # Replace with the desired file name
+        'name': f"{name}.md",  # Replace with the desired file name
         'parents': [folder_id] if folder_id else []
     }
 
